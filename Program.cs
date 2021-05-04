06643f50da54a826da8919c0a495218174d5cc7a
andyT994 <80651103+andyT994@users.noreply.github.com>
2021-05-04 15:49:05 +1000

    1st commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Challenge_1
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int DiceRandomNumb;
+
+            Random Dice = new Random();
+
+            List<int> dices = new List<int>();
+
+            int[] DiceNumb = new int[10];
+            Console.WriteLine(Dice);
+            for (int i = 0; i < 10; i++)
+    
+        {
+            DiceNumb[i] = Dice.Next(1, 6);
+        {
+            Console.WriteLine("Please Roll The Dice");
+            
+            Console.ReadKey();
+            DiceRandomNumb = Dice.Next(1, 6);
+            Console.WriteLine("You Roll A " + DiceRandomNumb);
+        }
+
+         Console.ReadKey();
+        }
+    } 
+ }
+}
